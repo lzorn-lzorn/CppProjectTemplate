61d8f8fd612aea3a43b338838600ee8ae41228d0
lizhuoran <liZhuoran1998@outlook.com>
2025-08-04 07:07:17 +0800

    Array2D Traits

--- intern/tools/array2d.hpp
+++ intern/tools/array2d.hpp
@@ -4,11 +4,43 @@
 #include <vector>
 namespace Tools {
 
+template <class Array>
+struct ConstIterator {};
+
+template <class Array>
+struct Iterator : public ConstIterator<Array> {
+
+};
+
+template <class Array>
+struct PointerTraits : public std::pointer_traits<Array> {};
+template <class Array>
+struct PointerTraits <ConstIterator<Array>>{};
+template <class Array>
+struct PointerTraits <Iterator<Array>>{};
+
+template <class ValueTp, class SizeTp, class DiffTp, class Pointer, class ConstPoint>
+struct IteratorTypes {
+    using value_type      = ValueTp;
+    using size_type       = SizeTp;
+    using difference_type = DiffTp;
+    using pointer         = Pointer;
+    using const_pointer   = ConstPoint;
+};
+
 template <typename Tp, class Allocator=std::allocator<Tp>>
 class Array2D : public std::vector<Tp, Allocator>{
 public:
-    using value_type = Tp;
-    using base = std::vector<Tp, Allocator>;
+    using base            = std::vector<Tp, Allocator>;
+    using value_type      = base::value_type;
+    using allocator_type  = base::allocator_type;
+    using pointer         = base::pointer;
+    using const_pointer   = base::const_pointer;
+    using reference       = base::reference;
+    using const_reference = base::const_reference;
+    using size_type       = base::size_type;
+    using difference_type = base::difference_type;
+    
 public:
     /* 使用原本vector的方法 */
     using base::empty;
@@ -28,37 +60,52 @@ public:
     bool operator==(const Array2D& other) const;
 
 private:
-    class Iterator {
-        typename base::iterator base_iter;
-        uint32_t col_;
-    public:
-        using iterator_category = std::random_access_iterator_tag;
-        using value_type = Tp;
-        using difference_type = std::ptrdiff_t;
-        using pointer = Tp*;
-        using reference = Tp&;
 
-        Iterator(typename base::iterator it, uint32_t col) 
-            : base_iter(it), col_(col) {}
-
-        reference operator*() { return *base_iter; }
-        Iterator& operator++() { ++base_iter; return *this; }
-        bool operator!=(const Iterator& other) const {
-            return base_iter != other.base_iter;
-        }
-        // 其他迭代器操作...
-        value_type& operator[](const uint32_t col);
-        value_type& operator[](const uint32_t col) const;
-    };
 
 public:
-    Iterator begin();
-    Iterator end();
-    Iterator operator[](const uint32_t row);
-    Iterator begin() const;
-    Iterator end() const;
-    Iterator operator[](const uint32_t row) const;
+
 private:
     uint32_t row, col;
 };
+
+template <class Tp, class Alloc>
+bool operator==(const Array2D<Tp, Alloc>& Left, const Array2D<Tp, Alloc>& Right){
+
+    return true;
+}
+template <class Tp, class Alloc>
+bool operator!=(const Array2D<Tp, Alloc>& Left, const Array2D<Tp, Alloc>& Right){
+
+    return true;
+}
+template <class Tp, class Alloc>
+bool operator<(const Array2D<Tp, Alloc>& Left, const Array2D<Tp, Alloc>& Right){
+
+    return true;
+}
+template <class Tp, class Alloc>
+bool operator>(const Array2D<Tp, Alloc>& Left, const Array2D<Tp, Alloc>& Right){
+
+    return true;
+}
+template <class Tp, class Alloc>
+bool operator<=(const Array2D<Tp, Alloc>& Left, const Array2D<Tp, Alloc>& Right){
+
+    return true;
+}
+template <class Tp, class Alloc>
+bool operator>=(const Array2D<Tp, Alloc>& Left, const Array2D<Tp, Alloc>& Right){
+
+    return true;
+}
+template <class Tp, class Alloc>
+void swap(Array2D<Tp, Alloc>& Left, Array2D<Tp, Alloc>& Right){}
+
+template <class Tp, class Alloc, class ValTp>
+constexpr Array2D<Tp, Alloc>::size_type erase(Array2D<Tp, Alloc>& array2d, const ValTp& val){}
+
+template <class Tp, class Alloc, class Predicate>
+constexpr Array2D<Tp, Alloc>::size_type erase_if(Array2D<Tp, Alloc>& array2d, Predicate pred){}
+
+
 }
