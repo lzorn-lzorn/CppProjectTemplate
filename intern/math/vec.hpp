a2d9851c37420b7a16f1e920a044c0486aaf6b52
lizhuoran <liZhuoran1998@outlook.com>
2025-08-04 07:24:13 +0800

    Vector 特化

--- intern/math/vec.hpp
+++ intern/math/vec.hpp
@@ -1,8 +1,16 @@
-#include <vector>
+#include <array>
 #include "traits.hpp"
 namespace Math {
 
+template <Arithmetic Tp, size_t Dimension>
+class Vector{
+public:
+
+private:
+    std::array<Tp, Dimension> vec;
+};
+
 template <Arithmetic Tp>
-class Vector{};
+class Vector<Tp, 1>{};
 
 }
