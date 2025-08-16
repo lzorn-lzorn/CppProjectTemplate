94e499e03b02ef28b0bb1b5a3cc8ef24d3600593
lizhuoran <liZhuoran1998@outlook.com>
2025-08-17 06:49:34 +0800

    array2d 核心方法

--- intern/tools/array2d.hpp
+++ intern/tools/array2d.hpp
@@ -1,55 +1,26 @@
 #pragma once
 #include <cstdint>
 #include <memory>
+#include <stdexcept>
 #include <vector>
+#include <string>
+#include <functional>
+#include <format>
 namespace Tools {
 
-template <class Array>
-struct ConstIterator {};
-
-template <class Array>
-struct Iterator : public ConstIterator<Array> {
-
-};
-
-template <class Array>
-struct PointerTraits : public std::pointer_traits<Array> {};
-template <class Array>
-struct PointerTraits <ConstIterator<Array>>{};
-template <class Array>
-struct PointerTraits <Iterator<Array>>{};
-
-template <class ValueTp, class SizeTp, class DiffTp, class Pointer, class ConstPoint>
-struct IteratorTypes {
-    using value_type      = ValueTp;
-    using size_type       = SizeTp;
-    using difference_type = DiffTp;
-    using pointer         = Pointer;
-    using const_pointer   = ConstPoint;
-};
-
-template <typename Tp, class Allocator=std::allocator<Tp>>
-class Array2D : public std::vector<Tp, Allocator>{
+template <typename Ty, class Allocator=std::allocator<Ty>>
+class Array2D{
+private:
+    using real_array_type = std::vector<std::reference_wrapper<Ty>, Allocator>;
 public:
-    using base            = std::vector<Tp, Allocator>;
-    using value_type      = base::value_type;
-    using allocator_type  = base::allocator_type;
-    using pointer         = base::pointer;
-    using const_pointer   = base::const_pointer;
-    using reference       = base::reference;
-    using const_reference = base::const_reference;
-    using size_type       = base::size_type;
-    using difference_type = base::difference_type;
+    using value_type = real_array_type::value_type;
+    using size_type = real_array_type::size_type;
+    using allocator_type = real_array_type::allocator_type;
     
-public:
-    /* 使用原本vector的方法 */
-    using base::empty;
-    using base::size;
-    using base::clear;
-
-    /* 禁用原本vector的方法 */
 
 public:
+    Array2D() 
+        : row(1), col(0) {}
     Array2D(const uint32_t row, const uint32_t col) 
         : row(row), col(col) {}
     Array2D(const Array2D&) = default;
@@ -57,55 +28,60 @@ public:
     Array2D& operator=(const Array2D&) = default;
     Array2D& operator=(Array2D&&) = default;
     ~Array2D() = default;
-    bool operator==(const Array2D& other) const;
 
-private:
+    bool operator==(const Array2D& other) const{
+        return array1d == other.array1d && row == other.row && col == other.col;
+    }
 
+    bool operator!=(const Array2D& other) const{
+        return !(*this == other);
+    }
 
+    std::vector<Ty> operator[](const uint32_t row) const noexcept{
+        std::vector<Ty> row_ {};
+        row_.reserve(col);
+        for (uint32_t col = 0; col < this->col; ++col) {
+            row_.emplace_back(array1d[row * this->col + col]);
+        }
+        return row_;
+    }
+    std::vector<Ty> GetRow(const uint32_t row) const {
+        if (row >= this->row) {
+            std::string err_msg = std::format("Row index out of bounds: row={}, this->row={}", row, this->row);
+            throw std::out_of_range(err_msg);
+        }
+        return operator[](row);
+    }
+    value_type At(const uint32_t row, const uint32_t col) const {
+        if (IsOutOfBoundary(row, col)){
+            std::string err_msg = std::format(
+                "Index out of bounds: row={}, col={}, this->row={}, this->col={}, Index = row * this->col + col = {}, boundary is {}",
+                row, col, this->row, this->col, row * this->col + col, array1d.size()
+            );
+            throw std::out_of_range(err_msg);
+        }
+        return array1d[row * this->col + col];
+    }
 public:
 
+
 private:
-    uint32_t row, col;
+    struct Coordinate {
+        uint64_t x, y;
+    };
+    bool IsOutOfBoundary(const uint32_t row, const uint32_t col) const noexcept{
+        return !(row < this->row && col < this->col && (array1d.size() > (row * this->col + col)));
+    }
+    uint64_t GetIndex(const uint32_t row, const uint32_t col) const noexcept {
+        return row * this->col + col;
+    }
+    Coordinate GetXY(const uint64_t num) const noexcept {
+        return { num / col, num % col };
+    }
+
+private:
+    std::vector<std::reference_wrapper<Ty>, Allocator> array1d;
+    uint32_t row{ 1 }, col{ 0 };
 };
-
-template <class Tp, class Alloc>
-bool operator==(const Array2D<Tp, Alloc>& Left, const Array2D<Tp, Alloc>& Right){
-
-    return true;
-}
-template <class Tp, class Alloc>
-bool operator!=(const Array2D<Tp, Alloc>& Left, const Array2D<Tp, Alloc>& Right){
-
-    return true;
-}
-template <class Tp, class Alloc>
-bool operator<(const Array2D<Tp, Alloc>& Left, const Array2D<Tp, Alloc>& Right){
-
-    return true;
-}
-template <class Tp, class Alloc>
-bool operator>(const Array2D<Tp, Alloc>& Left, const Array2D<Tp, Alloc>& Right){
-
-    return true;
-}
-template <class Tp, class Alloc>
-bool operator<=(const Array2D<Tp, Alloc>& Left, const Array2D<Tp, Alloc>& Right){
-
-    return true;
-}
-template <class Tp, class Alloc>
-bool operator>=(const Array2D<Tp, Alloc>& Left, const Array2D<Tp, Alloc>& Right){
-
-    return true;
-}
-template <class Tp, class Alloc>
-void swap(Array2D<Tp, Alloc>& Left, Array2D<Tp, Alloc>& Right){}
-
-template <class Tp, class Alloc, class ValTp>
-constexpr Array2D<Tp, Alloc>::size_type erase(Array2D<Tp, Alloc>& array2d, const ValTp& val){}
-
-template <class Tp, class Alloc, class Predicate>
-constexpr Array2D<Tp, Alloc>::size_type erase_if(Array2D<Tp, Alloc>& array2d, Predicate pred){}
-
 
 }
