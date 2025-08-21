d4bee06697fa2aa8d919e817bb3a242e0c94067d
Lzorn <lzr18535482977@gmail.com>
2025-08-21 17:21:00 +0800

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,22 +1,28 @@
-#include <benchmark/benchmark.h>
+// #include <benchmark/benchmark.h>
 
-// 一个简单的基准函数示例
-static void BM_StringCreation(benchmark::State& state) {
-    for (auto _ : state) {
-        std::string empty_string;
-    }
+// // 一个简单的基准函数示例
+// static void BM_StringCreation(benchmark::State& state) {
+//     for (auto _ : state) {
+//         std::string empty_string;
+//     }
+// }
+// BENCHMARK(BM_StringCreation);
+
+// // 你可以添加更多的基准测试函数
+// static void BM_VectorPushBack(benchmark::State& state) {
+//     for (auto _ : state) {
+//         std::vector<int> v;
+//         for (int i = 0; i < state.range(0); ++i) {
+//             v.push_back(i);
+//         }
+//     }
+// }
+// BENCHMARK(BM_VectorPushBack)->Arg(1000);
+
+// BENCHMARK_MAIN();
+#include <SDL3/SDL.h>
+#include <iostream>
+int main(){
+    std::cout << "Hello, SDL3!" << std::endl;
+    return 0;
 }
-BENCHMARK(BM_StringCreation);
-
-// 你可以添加更多的基准测试函数
-static void BM_VectorPushBack(benchmark::State& state) {
-    for (auto _ : state) {
-        std::vector<int> v;
-        for (int i = 0; i < state.range(0); ++i) {
-            v.push_back(i);
-        }
-    }
-}
-BENCHMARK(BM_VectorPushBack)->Arg(1000);
-
-BENCHMARK_MAIN();
