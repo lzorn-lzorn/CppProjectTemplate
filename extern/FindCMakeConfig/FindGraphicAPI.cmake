2793a54df42bcfa4afaaf6fee4370fd2749fc260
Lzorn <lzr18535482977@gmail.com>
2025-08-22 09:55:30 +0800

    Update FindGraphicAPI.cmake
    补充关于 glm  的注释

--- extern/FindCMakeConfig/FindGraphicAPI.cmake
+++ extern/FindCMakeConfig/FindGraphicAPI.cmake
@@ -1,5 +1,6 @@
 find_package(Vulkan REQUIRED)
 if (Vulkan_FOUND)
+    # Vulkan_INCLUDE_DIR 在下载时同时包含了 glm/glm.hpp
     target_include_directories(${PROJECT_NAME} PRIVATE ${Vulkan_INCLUDE_DIR})
     target_link_libraries(${PROJECT_NAME} PRIVATE Vulkan::Vulkan)
 else()
