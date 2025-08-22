bca4200cc9620c44941c03e17fd091136b82c7dd
Lzorn <lzr18535482977@gmail.com>
2025-08-22 15:20:04 +0800

    Update context.h

--- intern/render/context.h
+++ intern/render/context.h
@@ -1,7 +1,21 @@
 #pragma once
 #include "vulkan/vulkan.hpp"
-#include "glm/glm.hpp"
+#include "vulkan/vulkan_handles.hpp"
+#include <memory>
 namespace Render {
 
+class VulkanContext final{
+public:
+    /* Initialize Vulkan context */ 
+    static void Init();
+    /* Cleanup Vulkan context */ 
+    static void Cleanup();
+    static VulkanContext& Instance();
+    ~VulkanContext();
+    vk::Instance vkInstance;
 
+private:
+    VulkanContext();
+    static std::unique_ptr<VulkanContext> instance;
+};
 }
