43bded4c50d3db180f062bfdd4dadd5c3df941ec
Lzorn <lzr18535482977@gmail.com>
2025-08-22 15:20:23 +0800

    Update context.cpp

--- intern/render/context.cpp
+++ intern/render/context.cpp
@@ -1,1 +1,37 @@
-#include "Context.h"
+#include "context.h"
+#include "vulkan/vulkan_core.h"
+#include "vulkan/vulkan_structs.hpp"
+#include <memory>
+
+namespace Render {
+std::unique_ptr<VulkanContext> VulkanContext::instance = nullptr;
+
+VulkanContext::VulkanContext() {
+    vk::InstanceCreateInfo info;
+    vk::ApplicationInfo appInfo;
+    /* 以下设置 vk 的信息(基本上都是版本信息, 不是很重要) */
+    appInfo.setApiVersion(VK_API_VERSION_1_3);
+    
+    /* 设置完成 */
+    info.setPApplicationInfo(&appInfo);
+    vkInstance = vk::createInstance(info);
+}
+
+VulkanContext::~VulkanContext() {
+    vkInstance.destroy();
+}
+
+void VulkanContext::Init() {
+    instance.reset(new VulkanContext());
+}
+
+void VulkanContext::Cleanup() {
+    instance.reset();
+}
+
+VulkanContext& VulkanContext::Instance() {
+
+    return *instance;
+}
+
+} // namespace Render
