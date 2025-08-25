06ba72b0387514f858d3bfba5a3a9085625225f5
Lzorn <lzr18535482977@gmail.com>
2025-08-25 20:38:20 +0800

    Add inline specifier to GetTimeToString and ValToString

--- intern/Print/PrintTools.hpp
+++ intern/Print/PrintTools.hpp
@@ -1,12 +1,15 @@
 #pragma once
 #include <chrono>
 #include <string>
+#include <type_traits>
+#include <TypeTraits.hpp>
 #if defined(__cpp_lib_format)
   #include <format>
 #endif
+#include <Proj.hpp>
 namespace Tools{
 using namespace std::chrono;
-inline std::string GetTimeToString(
+INLINE std::string GetTimeToString(
     /* 年月日时分秒 */
 #if defined(__cpp_lib_format)
     std::format_string<int, unsigned, unsigned, unsigned, unsigned, double> format,
@@ -62,7 +65,47 @@ inline std::string GetTimeToString(
  * @   AdvancedType: std::optional, std::variant, std::any
  */
 template <typename ValTy>
-inline std::string ValToString(const ValTy& val){
-
+INLINE std::string ValToString(const ValTy& val){
+    using TargetType = std::remove_cvref_t<ValTy>;
+    /* number */
+    if constexpr (std::is_arithmetic_v<TargetType>){
+        return std::to_string(val);
+    /* std::string */
+    } else if constexpr (std::is_same_v<std::string, TargetType>){
+        return val;
+    /* const char* */
+    } else if constexpr (std::is_same_v<char*, TargetType>){
+        return std::string(val);
+    } else {
+        return "";
+    }
 }
+
+/* 
+ * @function: 将一个指针类型的range转换为字符串
+ */
+template <typename PointTy>
+INLINE std::string PtrToString(PointTy* ptr){
+    using extractor = pointer_extractor<PointTy>;
+    using pointer = typename extractor::value_type;
+    using target_type = typename std::remove_cvref<PointTy>::type;
+
+    if constexpr (std::is_pointer<target_type>::value || is_smart_ptr<target_type>::value) {
+        const void * raw_ptr = extractor::get_ptr(ptr);
+        if (!raw_ptr){
+            return "nullptr";
+        }
+        const pointer& deref_val = extractor::deref(ptr);
+
+        /* 函数指针 */
+        if constexpr (std::is_invocable<pointer>::value){
+            return std::to_string(std::invoke(deref_val));
+        } else {
+        
+
+        }
+    }
+    return "";
+}
+
 }
