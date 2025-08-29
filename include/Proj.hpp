ae74011c10ad3836a00b024238d62fce9b54f8f3
lizhuoran <liZhuoran1998@outlook.com>
2025-08-30 07:38:22 +0800

    render cmakelists > dll export

--- include/Proj.hpp
+++ include/Proj.hpp
@@ -1,5 +1,49 @@
 #pragma once
 
+/*
+ * 使用CMake显示指定使用:
+ * Proj_EXPORTS: 导入/出win下的API
+ * 导出 C API, Useage:
+    #ifdef __cplusplus
+    extern "C" {
+    #endif
+
+    Proj_API void MYLIB_CALL hello_c();
+
+    #ifdef __cplusplus
+    }
+    #endif
+ * 不兼容 x86 的话, Proj_CALL 是无效的, 所以可以不用管
+ */ 
+#if defined(_WIN32) || defined(__CYGWIN__)
+    #if defined (Proj_STATIC)
+        #define Proj_API
+    #elif defined (Proj_EXPORTS)
+        #define Proj_API __declspec(dllexport)
+    #else
+        #define Proj_API __declspec(dllimport)
+    #endif
+
+    #if defined(_M_X64) || defined(__x86_64__)
+        #define Proj_CALL
+    #else
+        #if defined(Proj_USE_STDCALL)
+            #define Proj_CALL __stdcall
+        #else
+            #define Proj_CALL __cdecl
+        #endif
+    #endif
+#else
+    #if defined (Proj_STATIC)
+        #define Proj_API
+    #else
+        #if __GNUC__ >= 4 && defined (__GNUC__)
+            #define Proj_API __attribute__ ((visibility ("default")))
+        #else
+            #define Proj_API
+        #endif
+    #endif
+#endif
 
 #if defined(__cpp_inline_variables)
     #define INLINE inline
@@ -12,6 +56,7 @@
     #else
         #define INLINE 
     #endif
+    #define Proj_CALL
 #endif
 
 /* 
@@ -33,4 +78,6 @@
 #else
     #define CONSTEXPR
 #endif
+
+
 
