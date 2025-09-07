47a303b0c9113759108f21c9c092d4b368f58292
lizhuoran <liZhuoran1998@outlook.com>
2025-09-08 05:36:04 +0800

    Pineline 注解

--- Intern/Render/Pipeline.cpp
+++ Intern/Render/Pipeline.cpp
@@ -18,6 +18,12 @@ void RenderProcess::InitPipeline(int width, int height) {
 	// 2. Vertex Assembly
 	vk::PipelineInputAssemblyStateCreateInfo inputAssembly;
 	inputAssembly.setPrimitiveRestartEnable(false)
+				/* eLineList: 多个直线, 1,2,3,4 -> 1-2, 3-4 */
+				/* eLineStrip: 连接成一条线, 1,2,3,4 -> 1-2-3-4 */
+				/* ePointList: 仅仅是点, 1,2,3,4 -> 1, 2, 3, 4 */
+				/* eTriangleList: 连接成三角形, 1,2,3,4 -> 1-2-3, 1-3-4 */
+				/* eTriangleStrip: 连接成三角形带, 1,2,3,4 -> 1-2-3, 2-3-4 */
+				/* eTriangleFan: 连接成三角形扇, 1,2,3,4 -> 1-2-3, 1-3-4 */
 				 .setTopology(vk::PrimitiveTopology::eTriangleList);
 	pipelineCreateInfo.setPInputAssemblyState(&inputAssembly);
 
@@ -33,12 +39,14 @@ void RenderProcess::InitPipeline(int width, int height) {
 		{0, 0},
 		{static_cast<uint32_t>(width), static_cast<uint32_t>(height)}
 	);
-	viewportState.setScissors(rect);
+			/* 指定真正绘制到屏幕上的区域大小 */
+	viewportState.setScissors(rect); // 全部绘制
 	pipelineCreateInfo.setPViewportState(&viewportState);
 
 	// 5. Rasterization
 	vk::PipelineRasterizationStateCreateInfo rastInfo;
 	rastInfo.setRasterizerDiscardEnable(false)
+			/* 面剔除 eBack -> 背面剔除 */
 			.setCullMode(vk::CullModeFlagBits::eBack)
 			.setFrontFace(vk::FrontFace::eCounterClockwise)
 			.setPolygonMode(vk::PolygonMode::eFill)
@@ -47,11 +55,12 @@ void RenderProcess::InitPipeline(int width, int height) {
 
 	// 6. Multisampling
 	vk::PipelineMultisampleStateCreateInfo multisample;
+				/* 关闭超采样 */
 	multisample.setSampleShadingEnable(false)
 			   .setRasterizationSamples(vk::SampleCountFlagBits::e1);
 	pipelineCreateInfo.setPMultisampleState(&multisample);
 
-	// 7. Test
+	// 7. Test stencil test, depth test
 
 
 	// 8. Color Blending
@@ -64,6 +73,7 @@ void RenderProcess::InitPipeline(int width, int height) {
 				vk::ColorComponentFlagBits::eG |
 				vk::ColorComponentFlagBits::eB
 			);
+		/* 关闭颜色混合 */
 	blend.setLogicOpEnable(false)
 		 .setAttachments(attaches);
 	pipelineCreateInfo.setPColorBlendState(&blend);
